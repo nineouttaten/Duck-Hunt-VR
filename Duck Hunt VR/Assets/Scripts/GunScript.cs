1b081ac73b2b048f03b9435bc22290eda60bf1ec
Nikita Kochrinskiy <nik.koch.67@yandex.ru>
2021-07-06 11:24:38 +0300

    Test bullets limit added

--- Duck Hunt VR/Assets/Scripts/GunScript.cs
+++ Duck Hunt VR/Assets/Scripts/GunScript.cs
@@ -1,20 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GunScript : MonoBehaviour
 {
+    
     public float speed = 40;
     public GameObject bullet;
     public Transform barrel;
     public AudioSource audioSource;
-    public AudioClip audioClip;
+    public AudioClip fire;
+    public AudioClip noammmo;
+    public AudioClip reload;
     public Transform raycastOrigin;
     public LayerMask targetLayer;
+    public int maxammo = 8;
+    private int currentammo;
+
+    void Reload()
+    {
+        currentammo = maxammo;
+        audioSource.PlayOneShot(reload);
+    }
+
+    private void Update()
+    {
+        if (currentammo < 0)
+            audioSource.PlayOneShot(noammmo);
+
+        if (Vector3.Angle(transform.up, Vector3.up) > 100 && currentammo < maxammo)
+            Reload();
+    }
+
     public void Fire()
     {
         GameObject spawnedBullet = Instantiate(bullet, barrel.position, barrel.rotation);
+        
         spawnedBullet.GetComponent<Rigidbody>().velocity = speed * barrel.forward;
+        currentammo--;
         //audioSource.PlayOneShot(audioClip);
         Destroy(spawnedBullet, 2);
         FireRaycast();
@@ -31,7 +55,6 @@ public class GunScript : MonoBehaviour
             {
                 hit.transform.GetComponent<ITargetInterface>().TargetShot();
             }
-
         }
     }
 }
